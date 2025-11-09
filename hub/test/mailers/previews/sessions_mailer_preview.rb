0ab1fdf637d0e5f4b5cdadda31e6011b007fe25b
Jesse Spevack <jspevack@gmail.com>
2025-11-09 15:36:14 -0700

    fix: update mailer preview to use mock user object

--- hub/test/mailers/previews/sessions_mailer_preview.rb
+++ hub/test/mailers/previews/sessions_mailer_preview.rb
@@ -2,6 +2,12 @@
 class SessionsMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/sessions_mailer/magic_link
   def magic_link
-    SessionsMailer.magic_link
+    user = User.new(
+      id: 1,
+      email_address: "preview@example.com"
+    )
+    token = "preview_token_abc123xyz"
+
+    SessionsMailer.magic_link(user: user, token: token)
   end
 end
