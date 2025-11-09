0a8be3144882fc27be3c6a9442d433466086ee40
Jesse Spevack <jspevack@gmail.com>
2025-11-09 09:51:24 -0700

    Chore: Setting up deploy

---
+++ hub/config/environments/development.local.rb
@@ -0,0 +1,22 @@
+
+require "json"
+
+# Load secrets directly from Kamal
+begin
+  secrets_output = `kamal secrets print`
+
+  secrets_json_str = secrets_output.lines.find { |line| line.start_with?("SECRETS=") }&.split("=", 2)&.last
+
+  if secrets_json_str
+    # Remove escaping to parse JSON properly
+    secrets_json_clean = secrets_json_str.gsub("\\", "")
+
+    secrets = JSON.parse(secrets_json_clean)
+
+    # Map with full paths as seen in the secrets output
+    ENV["RESEND_API_KEY"] = secrets["keys/tts/add more/RESEND_API_KEY"] if secrets["keys/tts/add more/RESEND_API_KEY"]
+
+    all_loaded = ENV["RESEND_CLIENT_ID"]
+    raise "Missing Google API credentials" unless all_loaded
+  end
+end
