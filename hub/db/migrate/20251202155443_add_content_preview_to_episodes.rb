509a6ee68856a339357e794017fc9e8fa8de5274
Jesse Spevack <jspevack@gmail.com>
2025-12-02 09:36:39 -0700

    Use column default instead of separate backfill migration

--- hub/db/migrate/20251202155443_add_content_preview_to_episodes.rb
+++ hub/db/migrate/20251202155443_add_content_preview_to_episodes.rb
@@ -1,5 +1,5 @@
 class AddContentPreviewToEpisodes < ActiveRecord::Migration[8.1]
   def change
-    add_column :episodes, :content_preview, :text
+    add_column :episodes, :content_preview, :text, default: "No preview available"
   end
 end
--- hub/db/schema.rb
+++ hub/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_02_155443) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_02_161658) do
   create_table "episode_usages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "episode_count", default: 0, null: false
