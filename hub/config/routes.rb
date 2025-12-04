6f9c5950049848dec7d2aed41afa8a0a4cde33c0
Jesse Spevack <jspevack@gmail.com>
2025-12-04 13:22:32 -0700

    style: fix rubocop spacing in routes

--- hub/config/routes.rb
+++ hub/config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   resource :session
-  resource :settings, only: [:show, :update]
+  resource :settings, only: [ :show, :update ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
