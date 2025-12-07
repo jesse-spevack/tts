f20398659b7bfb43da464e3178e834277407a3ca
Jesse Spevack <jspevack@gmail.com>
2025-12-07 07:05:59 -0700

    Refactor episode downloads to use RESTful format extension
    Replace verb-style /episodes/:id/download route with format-based
    /episodes/:id.mp3 for cleaner REST semantics.
    
    - Register mp3 MIME type in initializer
    - Update show action to respond_to html and mp3 formats
    - Remove separate download action
    - Update views to use episode_path with format: :mp3

--- hub/app/models/episode.rb
+++ hub/app/models/episode.rb
@@ -30,6 +30,26 @@ class Episode < ApplicationRecord
     "https://storage.googleapis.com/#{bucket}/podcasts/#{podcast_id}/episodes/#{gcs_episode_id}.mp3"
   end
 
+  def download_url
+    return nil unless complete? && gcs_episode_id.present?
+
+    bucket_name = ENV.fetch("GOOGLE_CLOUD_BUCKET", "verynormal-tts-podcast")
+    file_path = "podcasts/#{podcast.podcast_id}/episodes/#{gcs_episode_id}.mp3"
+    filename = "#{title.parameterize}.mp3"
+
+    storage = Google::Cloud::Storage.new
+    bucket = storage.bucket(bucket_name)
+    file = bucket.file(file_path)
+
+    file.signed_url(
+      method: "GET",
+      expires: 300,
+      query: {
+        "response-content-disposition" => "attachment; filename=\"#{filename}\""
+      }
+    )
+  end
+
   private
 
   def broadcast_status_change
