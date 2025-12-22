277d92eed7b4c90bdbb2321832cd5153e2bbecde
Jesse Spevack <jspevack@gmail.com>
2025-12-22 12:52:32 -0700

    fix: Resolve delete episode UI and job execution issues (#61)
    - Fix delete icon alignment with share/download buttons using form_class: "contents"
    - Fix "Content missing" error after deletion with turbo_frame: "_top"
    - Add KAMAL_REGISTRY_PASSWORD to env.secret so DeleteEpisodeJob can authenticate with GCS
    - Add structured logging to DeleteEpisodeJob following app conventions

--- hub/app/jobs/delete_episode_job.rb
+++ hub/app/jobs/delete_episode_job.rb
@@ -2,20 +2,26 @@ class DeleteEpisodeJob < ApplicationJob
   queue_as :default
 
   def perform(podcast_id:, gcs_episode_id:)
+    Rails.logger.info "event=delete_episode_job_started podcast_id=#{podcast_id} gcs_episode_id=#{gcs_episode_id}"
+
     gcs = GcsUploader.new(podcast_id: podcast_id)
 
-    # Delete MP3
-    gcs.delete_file(remote_path: "episodes/#{gcs_episode_id}.mp3")
+    deleted = gcs.delete_file(remote_path: "episodes/#{gcs_episode_id}.mp3")
+    Rails.logger.info "event=delete_episode_mp3_deleted podcast_id=#{podcast_id} gcs_episode_id=#{gcs_episode_id} deleted=#{deleted}"
 
-    # Update manifest
     manifest = EpisodeManifest.new(gcs)
     manifest.load
     manifest.remove_episode(gcs_episode_id)
     manifest.save
+    Rails.logger.info "event=delete_episode_manifest_updated podcast_id=#{podcast_id} gcs_episode_id=#{gcs_episode_id} remaining_episodes=#{manifest.episodes.size}"
 
-    # Regenerate feed
     podcast_config = YAML.load_file(Rails.root.join("../config/podcast.yml"))
     feed_xml = RssGenerator.new(podcast_config, manifest.episodes).generate
     gcs.upload_content(content: feed_xml, remote_path: "feed.xml")
+
+    Rails.logger.info "event=delete_episode_job_completed podcast_id=#{podcast_id} gcs_episode_id=#{gcs_episode_id}"
+  rescue StandardError => e
+    Rails.logger.error "event=delete_episode_job_failed podcast_id=#{podcast_id} gcs_episode_id=#{gcs_episode_id} error=#{e.class} message=#{e.message}"
+    raise
   end
 end
