fc5c6f0dee6433ce13597d152bd3f90f7be8ead0
Jesse Spevack <jspevack@gmail.com>
2025-12-26 15:21:36 -0700

    feat: all podcasts use internal TTS processing
    Remove external generator path:
    - Remove Cloud Tasks enqueuer and related services
    - Remove unused RssGenerator class
    - Simplify SubmitEpisodeForProcessing to always use internal path
    
    Removed files:
    - app/services/upload_episode_content.rb
    - app/services/enqueue_episode_processing.rb
    - app/services/cloud_tasks_enqueuer.rb
    - app/services/rss_generator.rb
    - test/services/upload_episode_content_test.rb
    - test/services/enqueue_episode_processing_test.rb
    - test/services/cloud_tasks_enqueuer_test.rb

--- hub/app/services/submit_episode_for_processing.rb
+++ hub/app/services/submit_episode_for_processing.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class SubmitEpisodeForProcessing
-  # Podcasts that use internal TTS processing (Hub) instead of external generator (Cloud Run)
-  INTERNAL_TTS_PODCAST_IDS = [
-    "podcast_195c82bf8eeb2aae"  # Jesse's podcast for testing
-  ].freeze
-
   def self.call(episode:, content:)
     new(episode: episode, content: content).call
   end
@@ -16,41 +11,19 @@ class SubmitEpisodeForProcessing
   end
 
   def call
-    if use_internal_tts?
-      process_internally
-    else
-      process_externally
-    end
-  end
-
-  private
-
-  attr_reader :episode, :content
-
-  def use_internal_tts?
-    INTERNAL_TTS_PODCAST_IDS.include?(episode.podcast.podcast_id)
-  end
-
-  def process_internally
-    Rails.logger.info "event=internal_tts_selected episode_id=#{episode.id} podcast_id=#{episode.podcast.podcast_id}"
+    Rails.logger.info "event=submit_episode_for_processing episode_id=#{episode.id} podcast_id=#{episode.podcast.podcast_id}"
 
     wrapped = wrap_content
     episode.update!(source_text: wrapped)
 
     GenerateAudioJob.perform_later(episode)
 
-    Rails.logger.info "event=internal_processing_enqueued episode_id=#{episode.id}"
-  end
-
-  def process_externally
-    staging_path = upload_content(wrap_content)
-
-    Rails.logger.info "event=staging_uploaded episode_id=#{episode.id} staging_path=#{staging_path} size_bytes=#{content.bytesize}"
-
-    enqueue_processing(staging_path)
-
     Rails.logger.info "event=processing_enqueued episode_id=#{episode.id}"
   end
+
+  private
+
+  attr_reader :episode, :content
 
   def wrap_content
     BuildEpisodeWrapper.call(
@@ -59,13 +32,5 @@ class SubmitEpisodeForProcessing
       tier: episode.user.tier,
       content: content
     )
-  end
-
-  def upload_content(wrapped_content)
-    UploadEpisodeContent.call(episode: episode, content: wrapped_content)
-  end
-
-  def enqueue_processing(staging_path)
-    EnqueueEpisodeProcessing.call(episode: episode, staging_path: staging_path)
   end
 end
