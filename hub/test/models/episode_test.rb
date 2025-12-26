af6495f2a9cf8fe61c17825430fbd207069dd712
Jesse Spevack <jspevack@gmail.com>
2025-12-26 16:43:32 -0700

    test: remove extraneous tests that test frameworks rather than app logic
    - Delete ApiClientTest: tested private methods via send()
    - Delete DeleteEpisodeJobTest: tested thin wrapper calls service
    - Trim EpisodeTest: remove validation/enum/delegate/gem tests
    
    Reduces test count from 465 to 447 while maintaining meaningful coverage.

--- hub/test/models/episode_test.rb
+++ hub/test/models/episode_test.rb
@@ -1,72 +1,6 @@
 require "test_helper"
 
 class EpisodeTest < ActiveSupport::TestCase
-  test "validates presence of title" do
-    episode = Episode.new(author: "Author", description: "Description", podcast: podcasts(:one))
-    episode.valid?
-
-    assert_includes episode.errors[:title], "can't be blank"
-  end
-
-  test "validates title length maximum" do
-    episode = Episode.new(
-      title: "a" * 256,
-      author: "Author",
-      description: "Description",
-      podcast: podcasts(:one)
-    )
-    episode.valid?
-
-    assert_includes episode.errors[:title], "is too long (maximum is 255 characters)"
-  end
-
-  test "validates presence of author" do
-    episode = Episode.new(title: "Title", description: "Description", podcast: podcasts(:one))
-    episode.valid?
-
-    assert_includes episode.errors[:author], "can't be blank"
-  end
-
-  test "validates author length maximum" do
-    episode = Episode.new(
-      title: "Title",
-      author: "a" * 256,
-      description: "Description",
-      podcast: podcasts(:one)
-    )
-    episode.valid?
-
-    assert_includes episode.errors[:author], "is too long (maximum is 255 characters)"
-  end
-
-  test "validates presence of description" do
-    episode = Episode.new(title: "Title", author: "Author", podcast: podcasts(:one))
-    episode.valid?
-
-    assert_includes episode.errors[:description], "can't be blank"
-  end
-
-  test "validates description length maximum" do
-    episode = Episode.new(
-      title: "Title",
-      author: "Author",
-      description: "a" * 1001,
-      podcast: podcasts(:one)
-    )
-    episode.valid?
-
-    assert_includes episode.errors[:description], "is too long (maximum is 1000 characters)"
-  end
-
-  test "status enum includes expected values" do
-    episode = episodes(:one)
-
-    assert_respond_to episode, :pending?
-    assert_respond_to episode, :processing?
-    assert_respond_to episode, :complete?
-    assert_respond_to episode, :failed?
-  end
-
   test "newest_first scope orders by created_at descending" do
     episodes(:one)
     newer_episode = Episode.create!(
@@ -124,56 +58,6 @@ class EpisodeTest < ActiveSupport::TestCase
     )
 
     assert_nil episode.audio_url
-  end
-
-  test "validates duration_seconds is positive" do
-    episode = episodes(:one)
-    episode.duration_seconds = -1
-    episode.valid?
-
-    assert_includes episode.errors[:duration_seconds], "must be greater than 0"
-  end
-
-  test "validates duration_seconds max is 24 hours" do
-    episode = episodes(:one)
-    episode.duration_seconds = 86_401
-    episode.valid?
-
-    assert_includes episode.errors[:duration_seconds], "must be less than or equal to 86400"
-  end
-
-  test "allows nil duration_seconds" do
-    episode = episodes(:one)
-    episode.duration_seconds = nil
-
-    assert episode.valid?
-  end
-
-  test "voice delegates to user" do
-    episode = episodes(:one)
-    episode.user.voice_preference = "sloane"
-
-    assert_equal "en-US-Standard-C", episode.voice
-  end
-
-  test "voice returns user default when no preference set" do
-    episode = episodes(:one)
-    episode.user.voice_preference = nil
-    episode.user.tier = :free
-
-    assert_equal "en-GB-Standard-D", episode.voice
-  end
-
-  test "has a prefixed id starting with ep_" do
-    episode = episodes(:two)
-    assert episode.prefix_id.present?
-    assert episode.prefix_id.start_with?("ep_")
-  end
-
-  test "can be found by prefix_id" do
-    episode = episodes(:two)
-    found = Episode.find_by_prefix_id(episode.prefix_id)
-    assert_equal episode, found
   end
 
   test "soft_delete! sets deleted_at to current time" do
