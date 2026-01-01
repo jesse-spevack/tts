cf7d8d600a2ffdecdfb057c034c07d0e43a94606
Jesse Spevack <jspevack@gmail.com>
2025-12-31 20:19:57 -0700

    feat: add Subscription model with status enum

---
+++ app/models/subscription.rb
@@ -0,0 +1,10 @@
+class Subscription < ApplicationRecord
+  belongs_to :user
+
+  enum :status, { active: 0, past_due: 1, canceled: 2 }
+
+  validates :stripe_customer_id, presence: true, uniqueness: true
+  validates :stripe_subscription_id, presence: true, uniqueness: true
+  validates :stripe_price_id, presence: true
+  validates :current_period_end, presence: true
+end
