5a43dc4b0d6c45a0ab006d0d2135ea69d305bdda
Jesse Spevack <jspevack@gmail.com>
2025-12-31 21:09:12 -0700

    test: add pricing section and plan param tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -43,4 +43,37 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     assert_empty cookies[:session_id].to_s
   end
+
+  test "verify with premium_monthly plan redirects to checkout" do
+    token = GenerateAuthToken.call(user: @user)
+
+    get auth_url, params: { token: token, plan: "premium_monthly" }
+
+    assert_redirected_to checkout_path(price_id: AppConfig::Stripe::PRICE_ID_MONTHLY)
+  end
+
+  test "verify with premium_annual plan redirects to checkout" do
+    token = GenerateAuthToken.call(user: @user)
+
+    get auth_url, params: { token: token, plan: "premium_annual" }
+
+    assert_redirected_to checkout_path(price_id: AppConfig::Stripe::PRICE_ID_ANNUAL)
+  end
+
+  test "verify without plan redirects to episodes" do
+    token = GenerateAuthToken.call(user: @user)
+
+    get auth_url, params: { token: token }
+
+    assert_redirected_to new_episode_path
+  end
+
+  test "create passes plan param to magic link" do
+    assert_emails 1 do
+      post session_url, params: { email_address: "test@example.com", plan: "premium_monthly" }
+    end
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_includes mail.body.to_s, "plan=premium_monthly"
+  end
 end
