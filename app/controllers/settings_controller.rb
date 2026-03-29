1aa95bb14cd065b10ccd9efa4922ccf09793ad70
Jesse Spevack <jspevack@gmail.com>
2026-03-29 13:48:58 -0600

    refactor: extract ListsConnectedOauthApplications service
    Move Doorkeeper token query and expiry filtering out of
    SettingsController into a service object.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,7 +7,7 @@ class SettingsController < ApplicationController
     @voices = Current.user.available_voices.map { |key| Voice.find(key) }
     @selected_voice = Current.user.voice_preference
     @email_ingest_address = Current.user.email_ingest_address
-    @connected_apps = connected_oauth_apps
+    @connected_apps = ListsConnectedOauthApplications.call(user: Current.user)
   end
 
   def update
@@ -26,27 +26,6 @@ class SettingsController < ApplicationController
   end
 
   private
-
-  def connected_oauth_apps
-    # Find OAuth apps with non-revoked tokens for this user.
-    # Filter expired tokens in Ruby since SQLite datetime arithmetic is non-standard.
-    tokens = Doorkeeper::AccessToken
-      .where(resource_owner_id: Current.user.id)
-      .where(revoked_at: nil)
-      .includes(:application)
-      .order(created_at: :desc)
-
-    # Group by app, keep only apps with at least one non-expired token
-    tokens
-      .select { |t| !t.expired? }
-      .group_by(&:application)
-      .map do |app, app_tokens|
-        {
-          app: app,
-          authorized_at: app_tokens.first&.created_at
-        }
-      end
-  end
 
   def settings_params
     permitted = params.permit(:voice, :email_episode_confirmation)
